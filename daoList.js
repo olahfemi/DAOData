f316b967613ffa78ca8b54ac4f1a72bfe585d9b5
Femi Olah <olahfemi@gmail.com>
2022-07-17 23:44:50 +0100

    add DAO list

---
+++ daoList.js
@@ -0,0 +1,120 @@
+const daoList = [
+    {
+        name: "UNISWAP",
+        description: "Uniswap is a Decentralized EXchange that generates automatic markets of liquidity asset pairs. It allows for flash transactions and liquidity allocation by specific price margins.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
+        website: "https://uniswap.org/",
+        twitter: "https://twitter.com/uniswap",
+        telegram: null,
+        discord: "https://discord.gg/FCfyBSbCU5",
+        discourse: "https://gov.uniswap.org/",
+        github: "https://github.com/Uniswap",
+        g_token: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+        ta: [
+                {investors: 0.1780}, 
+                {community: 0.6}, 
+                {team: 0.2151}, 
+                {advisors: 0.0069}
+            ]
+    },
+
+    {
+        name: "BitDAO",
+        description: "Bootstrapped by ByBit DEX and a massive launch auction, BitDAO aims to build, grant and partner towards a cross-ownership network of crypto projects promoting DeFi governance, treasury management and education.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/snapshots/spaces/bitdao.eth.png",
+        website: "https://www.bitdao.io/",
+        twitter: "https://twitter.com/BitDAO_Official",
+        telegram: "https://t.me/BitDAO_Official",
+        discord: "https://discord.com/invite/jTBC4BKnj7",
+        discourse: "https://discourse.bitdao.io/",
+        github: "https://github.com/BitDAOProtocol",
+        g_token: "0x1A4b46696b2bB4794Eb3D4c26f1c55F9170fa4C5",
+        ta: [
+                {private_sale: 0.05}, 
+                {launch_partner_rewards: 0.05}, 
+                {bitdao_treasury: 0.3}, 
+                {bybit_flexible: 0.15},
+                {bybit_locked: 0.45}
+            ]
+    },
+
+    {
+        name: "Gnosis",
+        description: "The Gnosis DAO community leads Gnosis' strategy in building and promoting prediction-market tools and protocols for finance & governance. It is the producer of MUCH used Gnosis Safe.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/gnosis.jpeg",
+        website: "https://gnosis.io/",
+        twitter: "https://twitter.com/gnosisPM",
+        telegram: null,
+        discord: "https://discord.com/invite/M39dTHQ",
+        discourse: "https://forum.gnosis.io/",
+        github: "https://github.com/gnosis",
+        g_token: "0x6810e776880C02933D47DB1b9fc05908e5386b96",
+        ta: [
+                {sold: 0.9}, 
+                {retained_by_company: 0.1} 
+            ]
+    },
+
+    {
+        name: "Polkadot",
+        description: "Polkadot is a fully- sharded, layer 0 blockchain technology. It offers the groundwork and security needed to easily build specialized-application layer 1 blockchains on top of it, called parachains. Parachains are connected to Polkadot's central Relay Chain, and benefit from its security and stacks. Blockchains that are not parachains can connect to the Polkadot network via Bridges. 
+        
+        Polkadot governance focuses on broad, protocol-level changes to the system, rather than specific internal ones. Relying on Substrate governance modules, decisions are approved by a stake-weighted voting event called a Referenda. Proposals may be from the public or made by the Council- an on-chain elected body that also manages the treasury and holds the power to halt harmful proposals.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/polkadot.png",
+        website: "https://polkadot.network/",
+        twitter: "https://twitter.com/Polkadot",
+        telegram: null,
+        discord: "https://discord.gg/wGUDt2p",
+        discourse: null,
+        github: "https://github.com/paritytech/polkadot",
+        g_token: null,
+        ta: [
+                {polkadot_auction: 0.5}, 
+                {further_prelaunch_distributions: 0.2},
+                {web3_foundation: 0.3} 
+            ]
+    },
+
+    {
+        name: "Merit Circle",
+        description: "Merit Circle is a Play2Earn DAO set to operate across different metaverse games ecosystems through investments, development and incubation of creative content, professional gaming community & training, and a marketplace for gaming NFT commodities. 
+        Merit Circle is currently governed through community signaling via Snapshot and multisig wallets. It plans to operate as a central DAO controlling different sub-DAOs, each working for continuity, scaling and earnings from a specific game's strategies, including collecting top NFTs of its particular game. 
+        
+        Merit Circle DAO currently holds treasury wallets on Ethereum (counted here) and also on BSC, Solana and Ronin blockchains, that are currently not served for their treasury information on DeepDAO.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/snapshots/spaces/meritcircle.eth.png",
+        website: "https://meritcircle.io/",
+        twitter: "https://twitter.com/MeritCircle_IO",
+        telegram: "https://t.me/meritcircle",
+        discord: "https://discord.com/invite/meritcircle",
+        discourse: null,
+        github: "https://github.com/Merit-Circle/",
+        g_token: "0x949d48eca67b17269629c7194f4b727d4ef9e5d6",
+        ta: [
+                {community_incentives: 0.294}, 
+                {team_and_advisors: 0.2},
+                {dao_treasury: 0.15},
+                {early_investors: 0.141},
+                {liquidity_rewards: 0.1}, 
+                {public_distribution: 0.075},
+                {retroactive_rewards: 0.04} 
+            ]
+    },
+
+    {
+        name: "OlympusDAO",
+        description: "The Olympus protocol sets up a decentralized reserve token that's collateralled by crypto assets rather than pegged to $US. It relies on diverse, protocol-owned over-collateralization and native compounding incentives.
+        Community governance is conducted by the protocol's staked governance token ($gOHM) token holders, through discussions and Snapshot voting signed by a multisig. It is currently preparing for on-chain community governance.",
+        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/snapshots/spaces/olympusdao.eth.png",
+        website: "https://www.olympusdao.finance/",
+        twitter: "https://twitter.com/OlympusDAO",
+        telegram: "https://t.me/OlympusTG",
+        discord: "https://discord.com/invite/6QjjtUcfM4",
+        discourse: null,
+        github: "https://github.com/OlympusDAO",
+        g_token: "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f",
+        ta: [
+                
+            ]
+    }
+]
+
